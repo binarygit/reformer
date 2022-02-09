36bd81d9e29cd109d65355e95a97702a6851c357
binarygit <inscription4@outlook.com>
2022-02-09 19:46:59 +0545

    finished with this

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,32 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
